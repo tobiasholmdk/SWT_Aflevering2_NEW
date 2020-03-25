5eb37a9c685b59f4984025fa136c5bdaef9038fa
Tobias Holm <201608355@post.au.dk>
2020-03-25 19:11:57 +0100

    DisplaySim Tests added

--- LadeskabTest/DisplaySimulatorUnitTest.cs
+++ LadeskabTest/DisplaySimulatorUnitTest.cs
@@ -19,7 +19,7 @@ namespace TestProject
         }
 
         [Test]
-        public void ValidateConsoleOutput()
+        public void ValidateConsoleOutput_IsReady()
         {
             using (StringWriter sw = new StringWriter())
             {
@@ -30,7 +30,65 @@ namespace TestProject
                 Assert.AreEqual(expected, sw.ToString());
             }
         }
-
-
+        [Test]
+        public void ValidateConsoleOutput_PresentRFID()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                _uut = new DisplaySimulator();
+                _uut.PresentRFID();
+                string expected = string.Format("Indlæs RFID{0}", Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
+        [Test]
+        public void ValidateConsoleOutput_ChargeError()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                _uut = new DisplaySimulator();
+                _uut.ChargeError();
+                string expected = string.Format("Tilslutningsfejl{0}", Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
+        [Test]
+        public void ValidateConsoleOutput_IsCharging()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                _uut = new DisplaySimulator();
+                _uut.IsCharging();
+                string expected = string.Format("Ladeskab optaget{0}", Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
+        [Test]
+        public void ValidateConsoleOutput_RFIDError()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                _uut = new DisplaySimulator();
+                _uut.RFIDError();
+                string expected = string.Format("RFID fejl, Prøv igen{0}", Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
+        [Test]
+        public void ValidateConsoleOutput_IsCharged()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                _uut = new DisplaySimulator();
+                _uut.IsCharged();
+                string expected = string.Format("Fjern telefon{0}", Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
     }
 }
